8042aad3f0e6e6280ddfed9b5f3829a19704d8cb
namhj94 <36985819+namhj94@users.noreply.github.com>
2019-10-16 00:15:16 +0900

    Update VoiceLogin.java

--- src/VoiceLogin/VoiceLogin.java
+++ src/VoiceLogin/VoiceLogin.java
@@ -16,7 +16,7 @@ public class VoiceLogin {
 	private JFrame frame;
 	private JTextField textField;
 	private JButton btnNewButton;
-	SecondPage sp = new SecondPage(); //�ι�° ������ �ν��Ͻ�����
+	SecondPage sp = new SecondPage(); // 두번째 페이지 인스턴스 생성
 	String snum;
 	VoiceLogin(){
 		Login();
@@ -24,16 +24,16 @@ public class VoiceLogin {
 	public void Login() {
 		frame = new JFrame();
 		frame.setBackground(UIManager.getColor("Button.highlight"));
-		frame.setTitle("������ �ڸ��߱�");
+		frame.setTitle("도서관 자리발권");
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("������ �ڸ��߱� �ý���");
+		JLabel lblNewLabel = new JLabel("도서관 자리발권 시스템");
 		lblNewLabel.setBounds(0, 32, 434, 41);
 		lblNewLabel.setForeground(UIManager.getColor("ProgressBar.selectionBackground"));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("����", Font.BOLD, 35));
+		lblNewLabel.setFont(new Font("굴림", Font.BOLD, 35));
 		frame.getContentPane().add(lblNewLabel);
 		
 		textField = new JTextField();
@@ -41,15 +41,15 @@ public class VoiceLogin {
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
-		JLabel lblNewLabel_1 = new JLabel("�й��� �Է��ϼ���.");
+		JLabel lblNewLabel_1 = new JLabel("학번을 입력하세요");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_1.setForeground(UIManager.getColor("ProgressBar.selectionBackground"));
-		lblNewLabel_1.setFont(new Font("����", Font.BOLD, 20));
+		lblNewLabel_1.setFont(new Font("굴림", Font.BOLD, 20));
 		lblNewLabel_1.setBounds(121, 98, 198, 34);
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		btnNewButton = new JButton("ENTER");
-		btnNewButton.setFont(new Font("����", Font.BOLD | Font.ITALIC, 20));
+		btnNewButton.setFont(new Font("굴림", Font.BOLD | Font.ITALIC, 20));
 		btnNewButton.setBounds(167, 207, 119, 23);
 		frame.getContentPane().add(btnNewButton);
 		btnNewButton.addActionListener(new ActionListener() {
@@ -60,11 +60,11 @@ public class VoiceLogin {
 		
 		frame.setVisible(true);
 	}
-	//�Է��� �й��� Ȯ���Ͽ� ��ϵǾ������� ������ ������ȣ�� ������ �ι�° �������� �Ѿ
+	//입력한 학번을 확인하여 등록되어있을 경우 개인의 고유번호를 갖고 두번째 페이지로 넘어감
 	public void checkTable() {
 		switch(textField.getText()) {
 		case  "1322039" : 
-			sp.Action(1); //���ڴ� ������ȣ
+			sp.Action(1); //괄호안의 숫자가 개인의 고유번호
 			break;
 		case  "1422011" : 
 			sp.Action(2);
@@ -79,7 +79,7 @@ public class VoiceLogin {
 			sp.Action(5);
 			break;
 		default :
-			JOptionPane.showMessageDialog(null, "�ٽ� �Է��ϼ���."); //�˾�
+			JOptionPane.showMessageDialog(null, "다시 입력하세요."); //팝업
 			break;
 		}
 	}
